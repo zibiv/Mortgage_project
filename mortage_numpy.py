e464b3e527426ef7c9f4212db3830d1dc731f3f6
GenIl <inmydreamsin@gmail.com>
2021-04-07 20:53:55 +0300

    Доделал вариант без использования сторонних библиотек и подтянул вариант с ними

--- mortage_numpy.py
+++ mortage_numpy.py
@@ -5,25 +5,44 @@ years =  int(input("How many years do you need to pay loan? "))
 borrowed =  int(input("Borrowed: "))
 interestRate =  float(input("Interest Rate: "))
 
+months = years * 12
+true_rate = interestRate / 12 #month's rate
+
 def paidThisMonth(interestRate, years, borrowed):
-    ptm = npf.pmt(interestRate / 12, years * 12, borrowed)
+    ptm = npf.pmt(true_rate, months, borrowed)
     return ptm
 
 
 pmt = paidThisMonth(interestRate, years, borrowed) #высчитывает непосредственно месячный платеж
-print(pmt)
-nper = npf.nper(interestRate/12, pmt, borrowed) #высчитывает кол-во месяцев для погашения ипотеки с заданным месячным платежем
+# print(pmt)
+nper = npf.nper(true_rate, pmt, borrowed) #высчитывает кол-во месяцев для погашения ипотеки с заданным месячным платежем
 print(np.round(nper))
-ipmt = npf.ipmt(interestRate/12, np.arange(years * 12) + 1, years * 12, borrowed)#часть ежемесячного платежа которая приходится на выплату процентов, по месячно
-print(ipmt)
-ppmt = npf.ppmt(interestRate/12, np.arange(years * 12) + 1, years * 12, borrowed)# часть которая приходится на выплату долга так же по месячно.
-print(ppmt)
-
-# header = '{:} -> {:} | {:} | {:}'
-fmt = '{0:2d} -> {1:8.2f} | {2:8.2f} | {3:8.2f}'
-for payment in (np.arange(years * 12) + 1):
-    index = payment - 1
-    borrowed = borrowed + ppmt[index]
-    print(fmt.format(payment, ppmt[index], ipmt[index], borrowed))
+ipmt = npf.ipmt(true_rate, np.arange(months) + 1, months, borrowed)#часть ежемесячного платежа которая приходится на выплату процентов, по месячно
+# print(ipmt)
+ppmt = npf.ppmt(true_rate, np.arange(months) + 1, months, borrowed)# часть которая приходится на выплату долга так же по месячно.
+# print(ppmt)
 
 
+def totalSumm(total_pem):
+    return round((total_pem * months), 2)
+def totalInterest(totalSumm):
+    return round((totalSumm + borrowed), 2)
+
+print("||||||||||||||||||||||||||||||||||||||||")
+print("Loan amout:%7.2f"%borrowed)
+print(f"Loan duration in months: {months}")
+print(f"Annunal Interest rate in percent: {interestRate*100}")
+print("Total Monthly Payment:%7.2f"%pmt)
+print(f"Total of {months} payments: {totalSumm(pmt)}")
+print(f"Over {months} months you will spend: {totalInterest(totalSumm(pmt))} in interest")
+wantFullReport = input("Do you want to make full report? (y/n) ")
+if wantFullReport == "y":
+    print("||||||||||||||||||||||||||||||||||||||||")
+    print("MONTH -> " + "  PRI | INTEREST| PRINCIPAL REMAINING")
+    fmt = '{0:2d} -> {1:8.2f} | {2:8.2f} | {3:8.2f}'
+    for payment in (np.arange(months) + 1):
+        index = payment - 1
+        borrowed = borrowed + ppmt[index]
+        print(fmt.format(payment, ppmt[index], ipmt[index], borrowed))
+else:
+    pass
--- mortgage.py
+++ mortgage.py
@@ -1,11 +1,10 @@
 import numpy as np
-
-years =  20#int(input("How many years do you need to pay loan? "))
-# salePrice =  int(input("Purchase price: "))
-borrowed = 150000# int(input("Borrowed: "))
-interestRate = 0.05 #float(input("Interest Rate: "))
+#Variables
+years =  int(input("Loan program in years: "))
+borrowed = int(input("Loan amount: "))
+interestRate = float(input("Interest Rate: "))
 months = years * 12
-true_rate = interestRate / 12
+true_rate = interestRate / 12 #month's rate
 
 def paidEveryMonth(interestRate, months, borrowed):
     total_pem = (borrowed * true_rate * ((true_rate + 1)**months)) / ((true_rate + 1)**months - 1)
@@ -15,26 +14,27 @@ def totalSumm(total_pem):
 def totalInterest(totalSumm):
     return round((totalSumm - borrowed), 2)
 def fullReport(total_pem, borrowed):
-    fmt = '{0:2d} -> {1:8.2f} | {2:8.2f} | {3:8.2f}'
+    print("||||||||||||||||||||||||||||||||||||||||")
+    print("MONTH -> P&I   " + "= PRINCIPAL + INTEREST| PRINCIPAL REMAINING")
+    fmt = '{0:2d} -> {4:8.2f} = {1:8.2f} + {2:8.2f} | {3:8.2f}'
     for index in range(1, months + 1):
         interest = borrowed * interestRate / 12
         principal = total_pem - interest
         borrowed -= principal
-        print(fmt.format(index, principal, interest, borrowed))
+        print(fmt.format(index, principal, interest, borrowed, total_pem))
 
-
+print("||||||||||||||||||||||||||||||||||||||||")
 print("Loan amout:%7.2f"%borrowed)
 print(f"Loan duration in months: {months}")
 print(f"Annunal Interest rate in percent: {interestRate*100}")
 tmp = paidEveryMonth(interestRate, months, borrowed)
-print(f"Total Monthly Payment: {tmp}")
+print("Total Monthly Payment:%7.2f"%tmp)
 print(f"Total of {months} payments: {totalSumm(tmp)}")
-print(f"Total Interest Paid: {totalInterest(totalSumm(tmp))}")
-whantFullReport = input("Do you want to make full report? (y/n)")
-if whantFullReport == "y":
+print(f"Over {months} months you will spend: {totalInterest(totalSumm(tmp))} in interest")
+wantFullReport = input("Do you want to make full report? (y/n) ")
+if wantFullReport == "y":
     fullReport(tmp, borrowed)
-else:
-    pass
+
 
 
 
